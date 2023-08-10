0b1b34c5d8146f856c88a0b9fa29c75867b939ae
wolbyte <wolbyte@gmail.com>
2023-08-10 18:09:52 +0330

    node_modules: add eslint + perform fixes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 
-module.exports = function(app) {
-  app.use(express.static(path.join (__dirname, "../public")))
+module.exports = function (app) {
+  app.use(express.static(path.join(__dirname, '../public')))
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 8080
 
-require("./routes")(app)
+require('./routes')(app)
 
 app.listen(port, () => {
   console.log(`HDGM listening on port ${port}`)
